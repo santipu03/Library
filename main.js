fbdcdd1da74903d2f7ad3f90c0f5fd600e0b7ef7
santipu_ <santipujolsau@gmail.com>
2022-03-25 21:23:35 +0100

    Implemented functions for adding new books to the library and displaying them

--- main.js
+++ main.js
@@ -8,43 +8,75 @@ const inputAuthor = document.querySelector(".title");
 const inputPages = document.querySelector(".title");
 const inputRead = document.querySelector(".title");
 const closeBtnModal = document.querySelector(".fa-xmark");
+const form = document.getElementById("form");
 
 
-
+// Modal Event Listeners
 addBtn.addEventListener("click", () => {
     modal.classList.add("show");
-})
-submitBook.addEventListener("click", ()=> {
-    console.log(inputTitle.textContent);
-    modal.classList.remove("show");
-    addBookToLibrary();
 })
 closeBtnModal.addEventListener("click", () => {
     modal.classList.remove("show");
 })
 
 
+const displayErrorMsg = () => {
+
+    // Verify that the message isn't already displayed, create it and display it in the form
+    if (!form.lastChild.textContent.startsWith("This")){
+        let message = document.createElement("div");
+        message.textContent = "This book has already been added";
+        message.style.color = "red";
+        form.appendChild(message);
+    }
+}
+
+
 
 myLibrary = [];
+let serialNumber = 0;
 
-function Book (author, title, pages, read) {
+function Book (author, title, pages, read, num) {
     this.author = author,
     this.title = title,
     this.pages = pages,
-    this.read = read
+    this.read = read,
+    this.num = num
 };
 
 
 const addBookToLibrary = (author, title, pages, read) => {
-    let book = new Book(author, title, pages, read);
-    myLibrary.push(book);
+
+    let book = new Book(author, title, pages, read, serialNumber);
+    
+
+
+    if (myLibrary.length == 0) {
+        myLibrary.push(book);
+        serialNumber++;
+        return book;
+    }
+    let push;
+    myLibrary.forEach(item => {
+        (item.title === book.title) ? push = false : push = true
+    });
+
+    if (push) {
+        myLibrary.push(book);
+        serialNumber++;
+        return book;
+    } else {
+        displayErrorMsg();
+    }
+
+
+    
 }
 
 
-addBookToLibrary("Tolkien","Lord of the Rings","2500","read");
-addBookToLibrary("Stephen King","The Shining","750","not read")
+// addBookToLibrary("Tolkien","Lord of the Rings","2500","read");
+// addBookToLibrary("Stephen King","The Shining","750","not read")
 
-console.log(myLibrary);
 
 
 const createCard = () => {
@@ -56,6 +88,7 @@ const createCard = () => {
     let newReadBtn = document.createElement("button");
 
     newCard.classList.add("card");
+    newCard.setAttribute("data-num", serialNumber)
     newTitle.classList.add("title");
     newAuthor.classList.add("author");
     newPages.classList.add("pages");
@@ -75,18 +108,36 @@ const createCard = () => {
 
 
 
-const displayBooks = (library) => {
-    library.forEach(book => {
-        let newCard = createCard();
-        newCard.children[0].textContent = book.title;
-        newCard.children[1].textContent = book.author;
-        newCard.children[2].textContent = book.pages;
-        newCard.children[3].textContent = book.read;
-        newCard.children[4].textContent = "Remove";
-        main.appendChild(newCard);
-        console.log(newCard);
-    });
+const displayBook = (book) => {
+
+    let newCard = createCard();
+    newCard.children[0].textContent = book.title;
+    newCard.children[1].textContent = book.author;
+    newCard.children[2].textContent = book.pages;
+    newCard.children[3].textContent = book.read;
+    newCard.children[4].textContent = "Remove";
+    main.appendChild(newCard);
+    console.log(newCard);
+
+    
 }
+
+
+
+submitBook.addEventListener("click", (e)=> {
+    e.preventDefault();
+    let transactionFormData = new FormData(form);
+    let readCheck;
+    (transactionFormData.get("read") == "on") ? readCheck = "read" : readCheck = "not read"
+
+    let newBook = addBookToLibrary(transactionFormData.get("author"),transactionFormData.get("title"),transactionFormData.get("pages"),readCheck);
+    displayBook(newBook);
+    moveAddButton();
+    modal.classList.remove("show");
+})
+
+
+
 
 
 
@@ -94,8 +145,6 @@ const moveAddButton = () => (addBtn.nextElementSibling !== null)? main.appendChi
 
 
 
-displayBooks(myLibrary);
-moveAddButton();
 
 
 
