40b50eccf7266a5eab921d283cd5fde8f2fd1fbe
santipu_ <santipujolsau@gmail.com>
2022-03-25 17:36:43 +0100

    HTML, style and little JS of the modal

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 // DOM Manipulation
 const main = document.querySelector(".main");
+const addBtn = document.querySelector(".add-btn");
+const modal = document.querySelector(".modal-overlay");
+const submitBook = document.querySelector(".submit-btn");
+const inputTitle = document.querySelector(".title");
+const inputAuthor = document.querySelector(".title");
+const inputPages = document.querySelector(".title");
+const inputRead = document.querySelector(".title");
+const closeBtnModal = document.querySelector(".fa-xmark");
 
 
 
+addBtn.addEventListener("click", () => {
+    modal.classList.add("show");
+})
+submitBook.addEventListener("click", ()=> {
+    console.log(inputTitle.textContent);
+    modal.classList.remove("show");
+    addBookToLibrary();
+})
+closeBtnModal.addEventListener("click", () => {
+    modal.classList.remove("show");
+})
 
 
 
@@ -21,31 +40,63 @@ const addBookToLibrary = (author, title, pages, read) => {
     myLibrary.push(book);
 }
 
-addBookToLibrary("JK Rowling","Harry Potter","250",false);
-addBookToLibrary("Tolkien","Lord of the Rings","2500",true);
-addBookToLibrary("Stephen King","The Shining","750",false)
+
+addBookToLibrary("Tolkien","Lord of the Rings","2500","read");
+addBookToLibrary("Stephen King","The Shining","750","not read")
 
 console.log(myLibrary);
 
 
-// creation of a new card
-let newCard = document.createElement("div");
-let newTitle = document.createElement("div");
-let newAuthor = document.createElement("div");
-let newPages = document.createElement("div");
-let newRemoveBtn = document.createElement("button");
-let newReadBtn = document.createElement("button");
+const createCard = () => {
+    let newCard = document.createElement("div");
+    let newTitle = document.createElement("div");
+    let newAuthor = document.createElement("div");
+    let newPages = document.createElement("div");
+    let newRemoveBtn = document.createElement("button");
+    let newReadBtn = document.createElement("button");
 
-newCard.classList.add("card");
-newTitle.classList.add("title");
-newAuthor.classList.add("author");
-newPages.classList.add("pages");
-newRemoveBtn.classList.add("button");
-newRemoveBtn.classList.add("remove");
-newReadBtn.classList.add("button");
-newReadBtn.classList.add("read");
-newCard.appendChild(newTitle);
-newCard.appendChild(newAuthor);
-newCard.appendChild(newPages);
-newCard.appendChild(newRemoveBtn);
-newCard.appendChild(newReadBtn);
+    newCard.classList.add("card");
+    newTitle.classList.add("title");
+    newAuthor.classList.add("author");
+    newPages.classList.add("pages");
+    newRemoveBtn.classList.add("button");
+    newRemoveBtn.classList.add("remove");
+    newReadBtn.classList.add("button");
+    newReadBtn.classList.add("read-btn");
+    newCard.appendChild(newTitle);
+    newCard.appendChild(newAuthor);
+    newCard.appendChild(newPages);
+    newCard.appendChild(newReadBtn);
+    newCard.appendChild(newRemoveBtn);
+    
+    return newCard;
+}
+
+
+
+
+const displayBooks = (library) => {
+    library.forEach(book => {
+        let newCard = createCard();
+        newCard.children[0].textContent = book.title;
+        newCard.children[1].textContent = book.author;
+        newCard.children[2].textContent = book.pages;
+        newCard.children[3].textContent = book.read;
+        newCard.children[4].textContent = "Remove";
+        main.appendChild(newCard);
+        console.log(newCard);
+    });
+}
+
+
+
+const moveAddButton = () => (addBtn.nextElementSibling !== null)? main.appendChild(addBtn): 0;
+
+
+
+displayBooks(myLibrary);
+moveAddButton();
+
+
+
+
